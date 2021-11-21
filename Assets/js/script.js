a64e7a4aaa9133e6d4a7c625cba56b059c8d427c
Debbie Hall <debleehall15@gmail.com>
2021-11-21 17:43:09 -0500

    pseudocoding

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword () {
+  console.log("button clicked");
+
+  // 1. prompt user with criteria
+  //    a. legth 8-128
+  //    b. lowercase
+  //    c. uppercase
+  //    d. numbers
+  //    e. special character
+  // 2. valadate the imput
+  // 3. gerate password
+
+
+
+  // 4. display the password on the page
+  return "generated password will go here."
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
