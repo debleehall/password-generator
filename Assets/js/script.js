da1e9e8530463a0bf00249e2df5b80eb8497b757
Debbie Hall <debleehall15@gmail.com>
2021-11-21 20:40:38 -0500

    arrays and variables

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,22 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword () {
-  console.log("button clicked");
+// character arrays
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var special = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-  // 1. prompt user with criteria
-  //    a. legth 8-128
-  //    b. lowercase
-  //    c. uppercase
-  //    d. numbers
-  //    e. special character
-  // 2. valadate the imput
+
+// varables
+var confirmLength = "";
+var confirmLowercase;
+var confirmUppercase;
+var confirmNumber;
+var confirmSpecial;
+
+function generatePassword () {
+  // Step 1. prompt user with criteria
+
+  // Step 2. valadate the imput
+
+
   // 3. gerate password
 
 
-
   // 4. display the password on the page
-  return "generated password will go here."
+
 }
 
 // Write password to the #password input
