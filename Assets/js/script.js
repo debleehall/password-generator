8752788f90a8cbf4f054ed3226fe405bbdd5f9af
Debbie Hall <debleehall15@gmail.com>
2021-11-21 20:43:48 -0500

    forgot to commit as i went and was going to go back to show my practice but decided not to

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -8,7 +8,7 @@ var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n
 var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 
-// varables
+// variables
 var confirmLength = "";
 var confirmLowercase;
 var confirmUppercase;
@@ -17,15 +17,44 @@ var confirmSpecial;
 
 function generatePassword () {
   // Step 1. prompt user with criteria
+  var confirmLength = window.prompt("How many characters is your password going to have?");
+    while(confirmLength <= 8 || confirmLength >= 128) {
+      window.alert("Password length must be between 8-128.");
+      var confirmLength = window.prompt("How many characters is your password going to have?");
+    }
 
-  // Step 2. valadate the imput
+  var confirmLowercase = confirm("Click confirm if you want to include lowercase letters.");
+  var confirmUppercase = confirm("Click confirm if you want to include UPPERCASE letters.");
+  var confirmNumber = confirm("Click confirm if you want to include numbers.");
+  var confirmSpecial = confirm("Click confirm if you want to include special characters.");
 
+  // Step 2. validate the input
+  var passwordCharacters = []
+
+  if (confirmLowercase) {
+    passwordCharacters = passwordCharacters.concat(lower);
+  }
+  if (confirmUppercase) {
+    passwordCharacters = passwordCharacters.concat(upper);
+  }
+  if (confirmNumber) {
+    passwordCharacters = passwordCharacters.concat(number);
+  }
+  if (confirmSpecial) {
+    passwordCharacters = passwordCharacters.concat(special);
+  }
 
   // 3. gerate password
+  var randomPassword = "";
+
+  for (var i = 0; i < confirmLength; i++) {
+    randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+    console.log(randomPassword);
+  }
 
 
   // 4. display the password on the page
-
+  return randomPassword;
 }
 
 // Write password to the #password input
